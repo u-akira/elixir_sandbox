1a0c8cbec0a793b69b3b09b95bbbec60e9c00bd2
u-akira <dragonranajp@yahoo.co.jp>
2023-08-07 22:51:42 +0900

    終端文字の追加

--- for_editor/lib/for_editor/english_sentence.ex
+++ for_editor/lib/for_editor/english_sentence.ex
@@ -17,21 +17,36 @@ defmodule ForEditor.EnglishSentence do
     input |> String.split() |> termination()
   end
 
+
+
   #終端文字の対応
-  #ピリオド以外の文字も対応したい ?, !
   defp termination([]), do: []
   defp termination([ head | tail]) do
-    if String.contains?(head, ".") do
-      split_terminate = head |> String.split(".")
-      Enum.map(split_terminate, fn(x) -> set_termination(x) end)
+    {result, terminating_character} = is_terminating_character(head)
+
+    if result do
+      split_terminate = head |> String.split(terminating_character)
+      Enum.map(split_terminate, fn(x) -> set_termination(x, terminating_character) end)
     else
       [head | termination(tail)]
     end
   end
 
-  defp set_termination(x) do
+
+  defp is_terminating_character(token) do
+    terminating_character = [".", "?", "!"]
+    {_, last_char} = String.split_at(token, -1)
+    if Enum.member?(terminating_character, last_char) do
+      {true, last_char}
+    else
+      {false, nil}
+    end
+  end
+
+
+  defp set_termination(x,terminating_character) do
     if String.length(x) == 0 do
-      String.replace(x,"",".")
+      String.replace(x,"",terminating_character)
     else
       x
     end
--- for_editor/test/english_sentence_test.exs
+++ for_editor/test/english_sentence_test.exs
@@ -9,4 +9,12 @@ defmodule EnglishSentenceTest do
     assert "This is a pen." |> to_list()  == ["This", "is", "a", "pen",  "."]
   end
 
+  test "'Are you hungry?' to list" do
+    assert "Are you hungry?" |> to_list()  == ["Are", "you", "hungry", "?"]
+  end
+
+  test "'I'm from Osaka!' to list" do
+    assert "I'm from Osaka!" |> to_list()  == ["I","'","m", "from", "Osaka", "!"]
+  end
+
 end
