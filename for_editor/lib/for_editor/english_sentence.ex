390b7729e4eb790191029ab00c1260118a8e829c
u-akira <dragonranajp@yahoo.co.jp>
2023-08-11 11:47:57 +0900

    to_listをリファクタリング

--- for_editor/lib/for_editor/english_sentence.ex
+++ for_editor/lib/for_editor/english_sentence.ex
@@ -1,54 +1,53 @@
 defmodule ForEditor.EnglishSentence do
   @moduledoc """
-  Documentation for `ForEditor`.
+  英文を管理するモジュールを提供します。
+  英文=大文字から始まりピリオドで終わる語群。
   """
 
   @doc """
-  Hello world.
+  英文をlistにします。
+  ".","?","."は終端文字として扱います。
+  "'"は前後の単語を分割します。
+  ## パラメータ
+  - input: 入力された文字列
 
   ## Examples
 
-      iex> ForEditor.hello()
-      :world
+      iex> ForEditor.EnglishSentence.to_list("This is a pen.")
+      ["This", "is", "a", "pen",  "."]
 
   """
-
   def to_list( input ) do
-    input |> String.split() |> termination()
+    input
+    |> String.split()
+    |> split_character()
+    |> Enum.filter(fn x -> x !== "" and x !== nil end)
   end
 
 
-
-  #終端文字の対応
-  defp termination([]), do: []
-  defp termination([ head | tail]) do
-    {result, terminating_character} = is_terminating_character(head)
+  def split_character([]), do: []
+  def split_character([ head | tail]) do
+    {result, split_character} = is_split_character(head)
 
     if result do
-      split_terminate = head |> String.split(terminating_character)
-      Enum.map(split_terminate, fn(x) -> set_termination(x, terminating_character) end)
+      [a, b] = head |> String.split(split_character)
+      [a, split_character, b | split_character(tail)]
     else
-      [head | termination(tail)]
+      [head | split_character(tail)]
     end
+
   end
 
 
-  defp is_terminating_character(token) do
-    terminating_character = [".", "?", "!"]
-    {_, last_char} = String.split_at(token, -1)
-    if Enum.member?(terminating_character, last_char) do
-      {true, last_char}
+  defp is_split_character(token) do
+    split_character = [".", "?", "!", "'"]
+    contained_chars = Enum.filter(split_character, fn char -> String.contains?(token, char) end)
+
+    if length(contained_chars) == 1 do
+      {true, hd(contained_chars)}
     else
       {false, nil}
     end
   end
 
-
-  defp set_termination(x,terminating_character) do
-    if String.length(x) == 0 do
-      String.replace(x,"",terminating_character)
-    else
-      x
-    end
-  end
 end
