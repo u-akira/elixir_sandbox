392402ae62e093f9a6f5060b98ee0e95b997f192
u-akira <dragonranajp@yahoo.co.jp>
2023-08-13 11:10:09 +0900

    パース処理を修正

--- for_editor/lib/for_editor/english_sentence.ex
+++ for_editor/lib/for_editor/english_sentence.ex
@@ -5,9 +5,11 @@ defmodule ForEditor.EnglishSentence do
   """
 
   @doc """
-  英文をlistにします。
-  ".","?","."は終端文字として扱います。
-  "'"は前後の単語を分割します。
+  英文をパースし、リストを作成します。
+
+  ・".","?","!"は終端文字として扱います。
+  ・"'"は省略記法の場合、分割します
+    "I'm", "They're"など
   ## パラメータ
   - input: 入力された文字列
 
@@ -20,8 +22,22 @@ defmodule ForEditor.EnglishSentence do
   def to_list( input ) do
     input
     |> String.split()
-    |> split_character()
+    |> parse()
+
+    #|> split_character()
     |> Enum.filter(fn x -> x !== "" and x !== nil end)
+  end
+
+  def parse([]), do: []
+  def parse([ head | tail]) do
+    {is_termination, split_character} = is_termination(head)
+
+    if is_termination do
+      [a, b] = head |> String.split(split_character)
+      [a, split_character, b | parse(tail)]
+    else
+      [head | parse(tail)]
+    end
   end
 
 
@@ -49,5 +65,30 @@ defmodule ForEditor.EnglishSentence do
       {false, nil}
     end
   end
+
+  def is_termination(token) do
+    termination_chars = [".", "?", "!"]
+    last_char_code = token |> String.to_charlist()
+                      |> List.last()
+    last_char = <<last_char_code::utf8>>
+
+    if Enum.member?(termination_chars, last_char) do
+      {true, last_char}
+    else
+      {false, nil}
+    end
+  end
+
+  """
+  defp is_terminating_character(token) do
+    terminating_character = [".", "?", "!"]
+    {_, last_char} = String.split_at(token, -1)
+    if Enum.member?(terminating_character, last_char) do
+      {true, last_char}
+    else
+      {false, nil}
+    end
+  end
+  """
 
 end
